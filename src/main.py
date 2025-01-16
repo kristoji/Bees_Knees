673d0daf73c92ca41592fa2f56f4a44b1658f10d
kristoji <tarantelli.kristjan@gmail.com>
2025-01-16 22:58:52 +0100

    interface

---
+++ src/main.py
@@ -0,0 +1,60 @@
+class Board:
+    game_conf: str
+
+    def __init__(self):
+        pass
+
+class Node:
+    turn: int
+    value: int
+    children: list
+    board_state: Board
+
+    def __init__():
+        return
+    
+    def append_child():
+        return
+    
+    # def perform_minimax():
+    #     return
+
+
+class Tree:
+    root: Node
+    # depth: int
+
+    def __init__(self):
+        pass
+
+    def build_limited_tree(root: Node, depth: int) -> Node:
+        return
+    
+def perform_minimax(node: Node) -> None:
+    return
+
+def evaluation_function(board: Board) -> int:
+    return
+
+def iterative_deepening(node: Node, max_time: int) -> Node:
+    return
+
+
+
+def display(board: Board) -> None:
+    return
+
+
+
+def do_move(board: Board, move: str) -> bool:
+    return
+
+def undo_move(board: Board, move: str) -> bool:
+    return
+
+def get_valid_moves(board: Board) -> list:
+    return
+
+
+if __name__ == '__main__':
+    pass
