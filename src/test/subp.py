5d476ed3063baadd5830c83e7bbac52f48146507
kristoji <tarantelli.kristjan@gmail.com>
2025-02-24 10:41:19 +0100

    test against mzinga

--- src/test/subp.py
+++ src/test/subp.py
@@ -1,10 +1,19 @@
 import subprocess
-import random
 
-ENGINE_PATH = "../../Mzinga.LinuxX64/MzingaEngine"
-DBG = True
-MAX_MOVES = 10
+# TODO: add check for win/loss/draw
 
+DBG = False
+MZINGA_PATH = "../../../MzingaEngine"
+OTHER_PATH = "../../dist/BeesKneesEngineAI"
+
+MAX_TURNS = 5
+DEPTH = 1
+TIME_TOTAL_SEC = 2
+
+
+TIME_H = TIME_TOTAL_SEC // 3600
+TIME_M = TIME_TOTAL_SEC // 60
+TIME_S = TIME_TOTAL_SEC % 60
 OK = "ok\n"
 
 def send(p: subprocess.Popen, command: str, dbg: bool=DBG) -> str:
@@ -35,31 +44,71 @@ def read_all(p: subprocess.Popen, dbg: bool=DBG) -> str:
             print(f"[<-] {line}")
     return output
 
-if __name__ == "__main__":
-    print("Starting interaction with MzingaEngine...\n")
-    with subprocess.Popen(
-        [ENGINE_PATH],
+def start_process(path) -> subprocess.Popen:
+    return subprocess.Popen(
+        [path],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         bufsize=1,
         universal_newlines=True,
-    ) as p:
-        read_all(p)
+    )
 
-        # send(p, "info")
-        send(p, "newgame Base+MLP")
+def end_process(p: subprocess.Popen) -> None:
+    p.stdin.close()
+    p.stdout.close()
+    p.stderr.close()
+    p.kill()
 
-        for i in range(MAX_MOVES):
-            moves = send(p, "validmoves").split(";")
-            move = random.choice(moves)
-            send(p, f"play {move}")
-            send(p, "bestmove time 00:00:02")
+def play_step(p1: subprocess.Popen, p2: subprocess.Popen, time: bool = False) -> str:
+    if time:
+        move = send(p1, f"bestmove time {TIME_H:02}:{TIME_M:02}:{TIME_S:02}")
+    else:
+        move = send(p1, f"bestmove depth {DEPTH}")
 
-        send(p, "exit")
+    move = move.strip().split("\n")[0]
+    name = p1.args[0].split("/")[-1][:12]
+    print(f"[{name}] \tplays: {move}")
+    
+    send(p1, f"play {move}")
+    return send(p2, f"play {move}")
+
+def check_end_game(out: str) -> None:
+    return "InProgress" != out.split(";")[1]
+
+
+if __name__ == "__main__":
+    
+    print("Starting interaction with MzingaEngine...\n")
+    mzinga: subprocess.Popen = start_process(MZINGA_PATH)
+    read_all(mzinga)
+
+    print(f"Starting interaction with {OTHER_PATH.split("/")[-1][:12]}...\n")
+    p = start_process(OTHER_PATH)
+    read_all(p)
+
+    send(mzinga, "newgame Base+MLP")
+    send(p, "newgame Base+MLP")
+
+    for i in range(MAX_TURNS):
+        out = play_step(mzinga, p)
+        if check_end_game(out):
+            print(out)
+            break
+
+        out = play_step(p, mzinga)
+        if check_end_game(out):
+            print(out)
+            break
         
-        p.stdin.close()
-        p.stdout.close()
-        p.stderr.close()
-        p.kill()
-        
+        print("Board: " + ";".join(out.split("\n")[0].split(";")[3:]) + "\n")
+
+    send(mzinga, "exit")
+    send(p, "exit")
+
+    
+    end_process(mzinga)
+    end_process(p)
+    print("Done.")
+    
+    
